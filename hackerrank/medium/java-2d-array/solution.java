ba342ee710f5de9d6015f6b7d1f932c6c233ea69
Omkawalkar <95124394+Omkawalkar@users.noreply.github.com>
2026-08-14 15:48:59 +0530

    ✅ Java 2D Array | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-14

--- hackerrank/medium/java-2d-array/solution.java
+++ hackerrank/medium/java-2d-array/solution.java
@@ -1,27 +1,49 @@
 import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 
 public class Solution {
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double pay = scanner.nextDouble();
-        scanner.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader =
+            new BufferedReader(new InputStreamReader(System.in));
 
-        // Write your code here.
-        Locale indiaLocale = new Locale("en", "IN");
+        int[][] arr = new int[6][6];
 
-NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
-NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
-NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
-NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        
-        System.out.println("US: " + us.format(pay));
-        System.out.println("India: " + india.format(pay));
-        System.out.println("China: " + china.format(pay));
-        System.out.println("France: " + france.format(pay));
+        for (int i = 0; i < 6; i++) {
+            String[] values = bufferedReader.readLine().split(" ");
+
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = Integer.parseInt(values[j]);
+            }
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        // There are 4 x 4 possible hourglasses
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                int sum =
+                    arr[i][j]     + arr[i][j + 1]     + arr[i][j + 2]
+                                  + arr[i + 1][j + 1]
+                    + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        System.out.println(maxSum);
+
+        bufferedReader.close();
     }
 }
