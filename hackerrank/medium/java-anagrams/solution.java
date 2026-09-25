05005bd9fac331a02da39721ec6df5179650098d
Omkawalkar <95124394+Omkawalkar@users.noreply.github.com>
2026-09-25 09:19:30 +0530

    ✅ Java Anagrams | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- hackerrank/medium/java-anagrams/solution.java
+++ hackerrank/medium/java-anagrams/solution.java
@@ -1,15 +1,35 @@
-import java.util.*;
 
-public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+    static boolean isAnagram(String a, String b) {
+           // If lengths are different, they cannot be anagrams
+        if (a.length() != b.length()) {
+            return false;
+        }
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        // Convert both strings to lowercase
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        // Frequency array for 26 English letters
+        int[] frequency = new int[26];
+
+        // Count characters in first string
+        for (int i = 0; i < a.length(); i++) {
+            frequency[a.charAt(i) - 'a']++;
+        }
+
+        // Remove characters using second string
+        for (int i = 0; i < b.length(); i++) {
+            frequency[b.charAt(i) - 'a']--;
+        }
+
+        // Check if all frequencies are zero
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
-}
+
